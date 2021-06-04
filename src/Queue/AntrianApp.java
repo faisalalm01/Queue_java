0d5d117bdfb0f3862744903549f891371e0b3935
faisalalm01 <faisalali2858@gmail.com>
2021-06-04 21:01:07 +0700

    tugas Queue_java

---
+++ src/Queue/AntrianApp.java
@@ -0,0 +1,23 @@
+package Queue;
+public class AntrianApp {
+
+    public static void main(String[] args) {
+        Antrian antrian = new Antrian(10);
+        antrian.masuk(40);
+        antrian.display();
+        antrian.masuk(33);
+        antrian.display();
+        System.out.println("nilai yang paling depan = "+ antrian.peekDepan());
+        antrian.masuk(60);
+        antrian.display();
+        System.out.println("yang diambil dari antrian = " + antrian.keluar());
+        antrian.display();
+        System.out.println("yang diambil dari antrian = " + antrian.keluar());
+        antrian.display();
+        antrian.masuk(54);
+        antrian.display();
+        System.out.println("nilai yang paling depan = "+ antrian.peekDepan());
+        
+    }
+    
+}
